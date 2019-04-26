7d904a5f16153a9c454cea637553108aa72d3996
Ivan <darkensson97@yandex.ru>
2019-04-26 12:33:21 +1000

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,25 @@
+// "use strict";
+a = 5;
+console.log(a);
+
+
+// let answer = +prompt("Есть ли вам 18?", "Да");
+// console.log(typeof(answer));
+// console.log("arr"+4);
+// console.log(4 + +"-object");
+
+let incr = 10,
+		decr = 10;
+
+
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5%2);
+// console.log("2"===2);
+
+let isChecked = false,
+		isClose = false;
+
+console.log(isChecked || !isClose);
